a4dc5e32c967d62052d558d59483499fbf0aa03b
Katey Almeida <77791996+klalmeida@users.noreply.github.com>
2022-03-27 16:24:36 -0400

    adding tests-end

---
+++ src/tests/dislike-screen.test.js
@@ -0,0 +1,57 @@
+import {MyDislikes} from "../components/profile/my-dislikes";
+import {screen, render} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+import {findAllTuitsDislikedByUser} from "../services/dislikes-service";
+import {UserList} from "../components/profile/user-list";
+
+import axios from "axios";
+jest.mock('axios');
+
+
+const MOCKED_USERS = [
+    {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
+    {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
+    {username: 'bob_ross', password: 'happyclouds', email: 'paintbrush@bob.com', _id: "345"},
+];
+const MOCKED_DISLIKES = [
+    {tuit: "bob's tuit", dislikedBy: 'ellen_ripley'},
+    {tuit: "ellen's tuit", dislikedBy: 'sarah_connor'},
+    {tuit: "sarah's tuit", dislikedBy: 'bob_ross'},
+];
+
+
+test('dislikes screen renders static dislike array', () => {
+    render(
+        <HashRouter>
+            <MyDislikes dislikes={MOCKED_DISLIKES}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/bob's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('dislikes screen renders async', async () => {
+    const dislikes = await findAllTuitsDislikedByUser('234');
+    render(
+        <HashRouter>
+            <UserList users={MOCKED_USERS}/>
+            <MyDislikes dislikes={dislikes}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/ellen's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+})
+
+test('dislike list renders mocked', async () => {
+    axios.get.mockImplementation(() =>
+        Promise.resolve({data: {dislikes: MOCKED_DISLIKES}}));
+
+    const response = await findAllTuitsDislikedByUser('234');
+    const dislikes = response.users;
+
+    render(
+        <HashRouter>
+            <MyDislikes dislikes={dislikes}/>
+        </HashRouter>);
+
+    const linkElement = screen.getByText(/ellen's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+});
---
+++ src/tests/like-screen.test.js
@@ -0,0 +1,57 @@
+import {MyLikes} from "../components/profile/my-likes";
+import {screen, render} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+import {findAllTuitsLikedByUser} from "../services/likes-service";
+import {UserList} from "../components/profile/user-list";
+
+import axios from "axios";
+jest.mock('axios');
+
+
+const MOCKED_USERS = [
+    {username: 'ellen_ripley', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
+    {username: 'sarah_conor', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
+    {username: 'bob_ross', password: 'happyclouds', email: 'paintbrush@bob.com', _id: "345"},
+];
+const MOCKED_LIKES = [
+    {tuit: "bob's tuit", likedBy: 'ellen_ripley'},
+    {tuit: "ellen's tuit", likedBy: 'sarah_connor'},
+    {tuit: "sarah's tuit", likedBy: 'bob_ross'},
+];
+
+
+test('likes screen renders static like array', () => {
+    render(
+        <HashRouter>
+            <MyLikes likes={MOCKED_LIKES}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/bob's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('likes screen renders async', async () => {
+    const likes = await findAllTuitsLikedByUser('234');
+    render(
+        <HashRouter>
+            <UserList users={MOCKED_USERS}/>
+            <MyLikes likes={likes}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/ellen's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+    })
+
+test('like list renders mocked', async () => {
+    axios.get.mockImplementation(() =>
+        Promise.resolve({data: {likes: MOCKED_LIKES}}));
+
+    const response = await findAllTuitsLikedByUser('234');
+    const likes = response.users;
+
+    render(
+        <HashRouter>
+            <MyLikes likes={likes}/>
+        </HashRouter>);
+
+    const linkElement = screen.getByText(/ellen's tuit/i);
+    expect(linkElement).toBeInTheDocument();
+});
